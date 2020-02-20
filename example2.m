a6b5e6f4ac0848840ca95aaf23a6d051e70eb62b
Ahmed Elsaharti <49626509+ahmed-elsaharti@users.noreply.github.com>
2020-02-20 15:10:52 -0600

    Added explanatory comments to example2

--- example2.m
+++ example2.m
@@ -1,5 +1,9 @@
 robot=InitializeRobot("COM6");
 disp(['Moving....'])
+
+% Move joint (3) to position (1000) with a given speed (100) and wait for the motion to be complete before proceeding to the next line
+% Expected input arguments: moveRobot(robot Object from initialize function, joint number, position (between 500 to 2500), speed)
+% Note that if the speed input argument is not provided the function defaults it to 750
 moveRobotWait(robot,3,1000,100);
 disp(['Motion Complete....'])
 clear robot
