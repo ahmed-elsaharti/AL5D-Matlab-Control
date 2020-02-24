5c029bab982aa60ef8a82f3442ec50ca41bccb7e
Ahmed Elsaharti <49626509+ahmed-elsaharti@users.noreply.github.com>
2020-02-24 14:02:48 -0600

    Added "group move" capability

--- RobotArm.m
+++ RobotArm.m
@@ -21,18 +21,27 @@ classdef RobotArm
        
     function [] = move(obj,channel,position,speed)
         %Defaulting speed to 750 if not specified
+        message="";
         if ~exist('speed','var')
             speed = 750;
         end
+        if length(speed)==1
+            speed=ones(1,length(channel))*speed;
+        end
+        if length(channel)~=length(position) || length(channel)~=length(speed)|| length(position)~=length(speed)
+            error('Size of input arrays not consistent')
+        end
         %Spitting out error if position is out of range
-        if position>2500 || position<500
-            error('Position out of range (500 to 2500)')
+        for ii=1:length(position)
+        if position(ii)>2500 || position(ii)<500
+            error("Position "+num2str(ii)+" out of range (500 to 2500)")
         end
         %Creating output message
-        message="#"+channel+"P"+position+"S"+speed;
+        message=message+"#"+channel(ii)+"P"+position(ii)+"S"+speed(ii);
+        end
         %Writing message to serial
         writeline(obj.SerialObj,message)
-    end
+    end   
     
     function [data] = read(obj,inputchannel)
         if ~exist('inputchannel','var')
@@ -50,18 +59,26 @@ classdef RobotArm
         if ~exist('speed','var')
             speed = 750;
         end
-        %Spitting out error if position is out of range
-        if position>2500 || position<500
-            error('Position out of range (500 to 2500)')
+        if length(speed)==1
+            speed=ones(1,length(channel))*speed;
         end
-        %Creating output message
-        message="#"+channel+"P"+position+"S"+speed;
-        %Writing message to serial
-        writeline(obj.SerialObj,message)
-        data='+';
-        while data=='+'
-            writeline(obj.SerialObj,"Q");
-            data = read(obj.SerialObj,1,"char");
+        if length(channel)~=length(position) || length(channel)~=length(speed)|| length(position)~=length(speed)
+            error('Size of input arrays not consistent')
+        end
+        for ii=1:length(position)
+            %Spitting out error if position is out of range
+            if position(ii)>2500 || position(ii)<500
+                error("Position "+num2str(ii)+" out of range (500 to 2500)")
+            end
+            %Creating output message
+            message="#"+channel(ii)+"P"+position(ii)+"S"+speed(ii);
+            %Writing message to serial
+            writeline(obj.SerialObj,message)
+            data='+';
+            while data=='+'
+                writeline(obj.SerialObj,"Q");
+                data = read(obj.SerialObj,1,"char");
+            end
         end
     end
    end
